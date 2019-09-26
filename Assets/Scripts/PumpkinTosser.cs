83c1d6dc74fc9aa3c84de29bc0ad14446e3a2015
Tameem Asif <tameem.asif02@gmail.com>
2019-09-26 10:41:01 -0400

    Day 3 done

---
+++ Assets/Scripts/PumpkinTosser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinTosser : MonoBehaviour {
+	public Rigidbody pumpkin;
+	public float initialSpeed = 2000f;
+	private float moveSpeed = 5f;
+	// Update is called once per frame
+	void Update ()
+	{
+		//allows user to move the camera with WASD keys
+		float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+		float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+		transform.Translate(deltaX, deltaY, 0);
+
+		if(Input.GetButtonUp("Fire1"))
+		{
+			Rigidbody pumpkinInstance = Instantiate(pumpkin, transform.position, transform.rotation) as Rigidbody;
+			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+			pumpkinInstance.AddForce(fwd*initialSpeed);
+		}
+	}
+}
